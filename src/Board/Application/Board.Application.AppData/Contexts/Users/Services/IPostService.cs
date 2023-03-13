44188498c97061a07726bd86d4e7d280acacd919
PavelDikunets <dikunets.pavel@gmail.com>
2023-03-14 01:42:29 +0300

    Добавил модель создания объявления
    Генерация файла документации
    
    Добавил сервис постов
    
    Добавил реализацию API

---
+++ src/Board/Application/Board.Application.AppData/Contexts/Users/Services/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using Board.Contracts.Posts;
+
+namespace Board.Application.AppData.Contexts.Users.Services;
+
+/// <summary>
+/// Сервис для работы с объявлениями.
+/// </summary>
+public interface IPostService
+{
+    /// <summary>
+    /// Создает объявление.
+    /// </summary>
+    /// <param name="model">Модель создания объявления.</param>
+    /// <param name="cancellation">Токен отмены операции.</param>
+    /// <returns>Модель объявления.</returns>
+    Task<CreatePostDto> AddPost(CreatePostDto model, CancellationToken cancellation);
+}
---
+++ src/Board/Application/Board.Application.AppData/Contexts/Users/Services/PostService.cs
@@ -0,0 +1,26 @@
+﻿using Board.Contracts.Posts;
+
+namespace Board.Application.AppData.Contexts.Users.Services;
+
+/// <inheritdoc />
+public class PostService : IPostService
+{
+    /// <inheritdoc />
+    public async Task<CreatePostDto> AddPost(CreatePostDto model, CancellationToken cancellation)
+    {
+        if (IsValid(model))
+        {
+            // Вызов репозитория для сохранения в БД.
+            
+            // возврат результата.
+            return await Task.Run(()=>model, cancellation);
+        }
+
+        return new CreatePostDto();
+    }
+
+    private bool IsValid(CreatePostDto model)
+    { // логика валидации...
+        return true;
+    }
+}
