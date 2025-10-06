53874dd1905254b3e6447290eb1001b33d08fa78
Pavel Dikuents <dikunets.pavel@gmail.com>
2025-10-06 21:18:49 +0300

    Добавил описание API, документацию и middleware

--- src/BulletinBoard/Hosts/BulletinBoard.Hosts.Api/Program.cs
+++ src/BulletinBoard/Hosts/BulletinBoard.Hosts.Api/Program.cs
@@ -1,17 +1,57 @@
+using BulletinBoard.Contracts.Announcements.Responses;
 using BulletinBoard.Infrastructure.ComponentRegistrar;
+using BulletinBoard.Infrastructure.DataAccess;
+using BulletinBoard.Infrastructure.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterAppServices();
 builder.Services.RegisterRepositories();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
 
-//builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "BulletinBoard API",
+        Version = "v1",
+        Description = "REST API для управления доской объявлений.",
+        Contact = new OpenApiContact
+        {
+            Name = "Pavel Dikunets",
+            Email = "dikunets.pavel@gmail.com",
+
+            Url = new Uri("https://github.com/PavelDikunets")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+
+    var assembliesWithXmlDocs = new[]
+    {
+        typeof(Program).Assembly,
+        typeof(AnnouncementResponse).Assembly
+    };
+    foreach (var assembly in assembliesWithXmlDocs)
+    {
+        var xmlFile = $"{assembly.GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        if (File.Exists(xmlPath)) options.IncludeXmlComments(xmlPath);
+    }
+});
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
